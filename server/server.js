d7b882a9c66ceb75cbd774f77f9014f068071527
Esteban (jipi) Coria <esteban.coria@despegar.com>
2022-03-25 09:15:58 -0300

    Update server errors and add logs and core

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@ require('dotenv/config')
 const express = require('express');
 const mongoose = require('./config/mongoose');
 const bodyParser = require('body-parser');
+const logger = require('./logger');
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 const app = express();
 
 const movementRoute = require('./routes/movements')
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use('/api/movements', movementRoute);
 
 // open mongoose connection
 mongoose.connect();
 
-app.listen(port);
+app.listen(port, () => logger.log('info', `API running on port ${port}`));
